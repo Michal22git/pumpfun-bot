78813fed1b6ede1a46e970022980b9102ebe97c0
micha <michalfiszer15@protonmail.com>
2024-06-01 13:57:14 +0200

    initial config

---
+++ bot/config.py
@@ -0,0 +1,52 @@
+import json
+import sys
+
+from solana.rpc.api import Client
+from solders.keypair import Keypair
+
+from bot.log import logger
+
+
+class Config(object):
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.load_config()
+            cls._instance.validate_config()
+        return cls._instance
+
+    def load_config(self):
+        try:
+            with open('config.json', 'r') as file:
+                config_data = json.load(file)
+                self.PRIVATE_KEY = config_data.get('PRIVATE_KEY')
+                self.DISCORD_WEBHOOK = config_data.get('DISCORD_WEBHOOK')
+                self.SLIPPAGE = config_data.get('SLIPPAGE')
+                self.TOKEN_ADDRESS = config_data.get('TOKEN_ADDRESS')
+                self.AMOUNT = config_data.get("AMOUNT")
+                self.RPC = config_data.get('RPC') or 'https://api.mainnet-beta.solana.com'
+        except Exception as e:
+            logger.error(f"Error loading config.json: {e}")
+            sys.exit(1)
+
+    def validate_config(self):
+        required_fields = ['PRIVATE_KEY', 'DISCORD_WEBHOOK', 'SLIPPAGE', 'TOKEN_ADDRESS', 'AMOUNT']
+        missing_fields = [field for field in required_fields if not getattr(self, field, None)]
+
+        if missing_fields:
+            logger.error(f"Required config fields missing: {', '.join(missing_fields)}")
+            sys.exit(1)
+
+    @property
+    def private_key(self):
+        return Keypair.from_base58_string(self.PRIVATE_KEY)
+
+    @property
+    def public_key(self):
+        return self.private_key.pubkey()
+
+    @property
+    def client(self):
+        return Client(self.RPC)
---
+++ main.py
@@ -0,0 +1,49 @@
+from InquirerPy import inquirer
+from pyfiglet import Figlet
+
+from bot.config import Config
+
+
+class Menu(Config):
+
+    def display_logo(self):
+        print("\033c", end="")
+        print(Figlet(font='slant').renderText('PumpFun Bot'))
+
+    def menu(self):
+        self.display_logo()
+        choices = inquirer.select(
+            message="Select option:",
+            choices=[
+                "PumpFun Sniper",
+                "Manage Wallet",
+                "Configuration",
+                "Exit"
+            ]
+        ).execute()
+
+        if choices == "PumpFun Sniper":
+            pass
+        elif choices == "Manage Wallet":
+            pass
+        elif choices == "Configuration":
+            print("Private Key: ", self.PRIVATE_KEY)
+            print("RPC: ", self.RPC)
+            print("Token Address: ", self.TOKEN_ADDRESS)
+            print("Amount (SOL):", self.AMOUNT)
+            print("Slippage (%):", self.SLIPPAGE)
+            print("Discord Webhook: ", self.DISCORD_WEBHOOK)
+
+            input("")
+
+            return self.menu()
+        elif choices == "Exit":
+            pass
+        else:
+            print("Invalid choice")
+            return self.menu()
+
+
+if __name__ == '__main__':
+    obj = Menu()
+    obj.menu()
